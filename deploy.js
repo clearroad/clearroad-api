f07e8c422542cb54f040c6da62eea03282cd5cd6
Guillaume Royer <guillaume.royer@clearroad.io>
2018-07-28 13:29:54 +0100

    chore: deploy js files

---
+++ deploy.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+
+if (!process.env.CI) {
+  try {
+    const dotenv = require('dotenv');
+    dotenv.config();
+  }
+  catch (err) {}
+}
+
+const azure = require('azure-storage');
+const blobService = azure.createBlobService();
+
+const createShare = (container) => {
+  return new Promise((resolve, reject) => {
+    blobService.createContainerIfNotExists(container, {
+      publicAccessLevel: 'blob'
+    }, (error, result) => {
+      if (!error) {
+        return resolve(result);
+      }
+      return reject(error);
+    });
+  });
+};
+
+const updloadFile = (container, file) => {
+  return new Promise((resolve, reject) => {
+    const filename = path.basename(file);
+    console.log(`\t- ${file}`);
+    blobService.createBlockBlobFromLocalFile(container, filename, file, (error, result) => {
+      if (!error) {
+        return resolve(result);
+      }
+      return reject(error);
+    });
+  });
+};
+
+const run = async () => {
+  try {
+    console.log('Uploading src folder...');
+    await createShare('api');
+    let directory = './src';
+    let files = fs.readdirSync(path.resolve(directory));
+    await Promise.all(files.map(file => updloadFile('api', path.resolve(directory, file))));
+
+    console.log('Uploading lib folder...');
+    await createShare('lib');
+    directory = './lib';
+    files = fs.readdirSync(path.resolve(directory));
+    await Promise.all(files.map(file => updloadFile('lib', path.resolve(directory, file))));
+
+    process.exit(0);
+  }
+  catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+run();
